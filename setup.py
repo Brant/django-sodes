5608b5d4a9b4ce8d4070f3aebb713036d00362be
Brant Steen <brant.steen@gmail.com>
2012-06-29 12:22:18 -0400

    Slicker setup

--- setup.py
+++ setup.py
@@ -1,44 +1,4 @@
-import os
-import sys
-
 from setuptools import setup, find_packages
-
-def fullsplit(path, result=None):
-    """
-    Split a pathname into components (the opposite of os.path.join) in a
-    platform-neutral way.
-    
-    Taken from Django's setup.py
-    """
-    if result is None:
-        result = []
-    head, tail = os.path.split(path)
-    if head == '':
-        return [tail] + result
-    if head == path:
-        return result
-    return fullsplit(head, [tail] + result)
-
-
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
-
-# Taken from Django's setup.py
-packages, data_files = [], []
-root_dir = os.path.dirname(__file__)
-if root_dir != "":
-    os.chdir(root_dir)
-
-for dirpath, dirnames, filenames in os.walk("sodes"):
-    for i, dirname in enumerate(dirnames):
-        if dirname.startswith('.'): del dirnames[i]
-    if '__init__.py' in filenames:
-        packages.append('.'.join(fullsplit(dirpath)))
-    elif filenames:
-        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])    
-
 
 
 
@@ -48,15 +8,18 @@ setup(
     url = 'http://github.com/Brant/django-sodes',
     license = 'GPL',
     description = "Podcast episodes for Django",
-    long_description = read('README'),
+    long_description = open('README').read(),
 
     author = 'Brant Steen',
     author_email = 'brant.steen@gmail.com',
+    
+    install_requires = ['setuptools', 'django', ],
+    
+    
+    packages = find_packages(exclude=('tests', )),
+    include_package_data = True,
     zip_safe = False,
-    packages = packages,
-    data_files = data_files,
-
-    install_requires = ['setuptools', 'django'],
+    
 
     classifiers = [
         'Development Status :: 3 - Alpha',
